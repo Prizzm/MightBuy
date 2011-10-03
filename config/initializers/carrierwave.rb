2df5248175d98b7022cb81f49990cbd349f8d3d1
Mitch Thompson <mitchkthompson@gmail.com>
2011-10-03 14:13:41 -0600

    Adding correct endpoint for s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,7 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                 => 'eu-west-1'
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
     }
     config.fog_directory  = 'prizzm-invites'
     config.cache_dir      = Rails.root.join("tmp/uploads")
