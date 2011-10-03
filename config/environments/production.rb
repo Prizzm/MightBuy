c7296f3c8dadd33ba6085ada48e8ba71b3d24ef0
Mitch Thompson <mitchkthompson@gmail.com>
2011-10-03 13:09:35 -0600

    Adding host to production mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ NewPrizzmCom::Application.configure do
   
   # Sendgrid
   config.action_mailer.delivery_method = :smtp
+  
   config.action_mailer.smtp_settings   = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
@@ -67,6 +68,10 @@ NewPrizzmCom::Application.configure do
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com'
+  }
+  
+  config.action_mailer.default_url_options = { 
+    :host => 'prizzm.com' 
   }
 
 end
