fef40531bf50955c6c1421fd76120fb318b8ec7b
Rahmal Conda <rahmal@gmail.com>
2012-03-02 14:57:01 -0800

    Putting response e-mail back on synchronous processing.

--- app/observers/response_observer.rb
+++ app/observers/response_observer.rb
@@ -6,7 +6,7 @@ class ResponseObserver < ActiveRecord::Observer
     end
     
     unless response.reply_to_email.blank?
-      Notifications.delay(:queue => 'Notifications').responded(response)
+      Notifications.responded(response).deliver
     end
   end
   
