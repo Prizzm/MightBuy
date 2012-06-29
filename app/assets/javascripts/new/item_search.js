3ac3d67b96664d9b1fe8e68253d6df925300c448
Amit Ambardekar <amitamb@gmail.com>
2012-06-29 21:16:45 +0530

    Some finishing touches with page title

--- app/assets/javascripts/new/item_search.js
+++ app/assets/javascripts/new/item_search.js
@@ -1,17 +1,19 @@
 $(function(){
   // When user clicks search icon in item url
   $("#item_url_search").click(function(e){
-	if ( $(this).data("status") != "open" ) {
-      $("#topic_url").autocomplete('enable');
-      $("#topic_url").autocomplete('search', $("#topic_subject").val());
-    }
-    else {
-      $("#topic_url").autocomplete('disable');
-      $("#topic_url").autocomplete('close');
+    if ( $("#topic_subject").val() ) {
+      if ( $(this).data("status") != "open" ) {
+        $("#topic_url").autocomplete('enable');
+        $("#topic_url").autocomplete('search', $("#topic_subject").val());
+      }
+      else {
+        $("#topic_url").autocomplete('disable');
+        $("#topic_url").autocomplete('close');
+      }
     }
     e.preventDefault();
   });
-  
+
   $(document).click(function(e){
     if( $(e.target).closest("#item_url_search").length == 0 ){
       $("#topic_url").autocomplete('disable');
@@ -26,7 +28,7 @@ $(function(){
 		var res = r.responseData.results;
 		if ( res.length ) {
           res = $.map(res, function(n, i){
-            return { label : n.unescapedUrl, value : n.unescapedUrl };
+            return { label : n.unescapedUrl, value : n.unescapedUrl, title : n.titleNoFormatting };
           });
         }
         response(res);
@@ -38,9 +40,20 @@ $(function(){
     },
     close: function(event, ui) {
       $.data($("#item_url_search").get(0), "status", "close");
-    }
-  });
-  
+    },
+    select : function(event, ui){
+      // set grab image URL
+      $(".image-selector-uploader #web input").val(ui.item.value);
+      $(".image-selector-uploader #web .get").click();
+    },
+    appendTo : "#item_url_search_autocomplete"
+  }).data( "autocomplete" )._renderItem = function( ul, item ) {
+     return $( "<li></li>" )
+      .data( "item.autocomplete", item )
+      .append( "<a>" + item.title + "<br><span style='font-size:12px;'>" + item.label + "</span></a>" )
+      .appendTo( ul );
+  };
+
   $("#topic_url").autocomplete({ disabled: true });
 
 });
