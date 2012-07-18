1fc12a324709aada4eee853dbc0be4c67a892855
Amit Ambardekar <amitamb@gmail.com>
2012-07-19 02:17:47 +0530

    Support for comma in price

--- config/initializers/product_scraper.rb
+++ config/initializers/product_scraper.rb
@@ -7,7 +7,7 @@ module ProductScraper
       price = nil
       return price if doc.blank?
       ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
-      prices = ic.iconv(doc.text).scan( /\$[0-9]+(?:\.[0-9]?[0-9]?)?/ ).collect { |p| p[1..-1] }
+      prices = ic.iconv(doc.text).scan( /\$[0-9\,]+(?:\.[0-9]?[0-9]?)?/ ).collect { |p| p[1..-1] }
       # get first non-zero price
       prices.each do |p|
         if p.to_f > 0
