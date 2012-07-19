14d2ad6a44f17a58ca7a49830602a92351416d5d
Sam Baumgarten <sbaumgarten@makrr.com>
2012-07-19 13:10:42 -0700

    Fixed Facebook

--- app/controllers/get_controller.rb
+++ app/controllers/get_controller.rb
@@ -1,15 +1,13 @@
 class GetController < ApplicationController
-  
-  def images    
+  def images 
     respond_to do |wants|
       wants.json { render :json => Scrape.images(params[:url]) }
     end
   end
   
-  def product
+  def product 
     respond_to do |wants|
       wants.json { render :json => Scrape.product(params[:url]) }
     end
   end
-  
 end
