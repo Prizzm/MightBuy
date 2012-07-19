ac7f5fd8803c951209f5a95e6659a2fbb4010a8c
Sam Baumgarten <sbaumgarten@makrr.com>
2012-07-18 20:18:37 -0700

    added_facebook

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -11,4 +11,5 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
   alias_method :twitter, :all
+  alias_method :facebook, :all
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -208,4 +208,5 @@ Devise.setup do |config|
   #   manager.default_strategies(:scope => :user).unshift :some_external_strategy
   # end
   config.omniauth :twitter, "LlSCGZ10wqQb96SSJQzw", "nHsYOZwO0kuGMlkCsRIy80IIjYDr0cZqUzeXOOHro"
+  config.omniauth :facebook, "461844450500925", "4c9659082e85db5f8e0b8db4122060e9"
 end
