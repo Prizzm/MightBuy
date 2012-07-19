4663a779bd679233335ffc6a869b2679b5c4131e
Sam Baumgarten <sbaumgarten@makrr.com>
2012-07-19 10:25:10 -0700

    ask_friends_button

--- app/controllers/social_controller.rb
+++ app/controllers/social_controller.rb
@@ -2,6 +2,19 @@ class SocialController < ApplicationController
   
   skip_filter :award_points
   
+  def askfriends
+    if current_user.facebook_uid then
+      me = FbGraph::User.me("AAAGkC4pp4T0BAI1upGuC5CX7l0nRZA4QIocuUtlnGLMB0TFV5gUszMfeAAR4TzGWkt4MSedHfFiEKh4QduvnwPK1IdsA1B7od6WEA7AZDZD")
+      action = me.og_action!(
+        "mightbuy:might_buy",
+        :product => "http://mightbuy.it#{url_for(Topic.find_by_shortcode(params[:sc]))}"
+      )
+      redirect_to "http://mightbuy.it#{url_for(Topic.find_by_shortcode(params[:sc]))}"
+    else
+      redirect_to "/users/auth/facebook"
+    end
+  end
+  
   def tweeted
     
     url   = Rack::Utils.parse_query(URI.parse(params[:url]).fragment)["url"]
@@ -24,28 +37,22 @@ class SocialController < ApplicationController
   end
   
   def recommended
-    if current_user.facebook_uid then
-      me = FbGraph::User.me("AAAGkC4pp4T0BAI1upGuC5CX7l0nRZA4QIocuUtlnGLMB0TFV5gUszMfeAAR4TzGWkt4MSedHfFiEKh4QduvnwPK1IdsA1B7od6WEA7AZDZD")
-      action = me.og_action!(
-        "mightbuy:might_buy",
-        :product => "http://mightbuy.it#{url_for(@topic)}"
-      )
+    
+    url   = CGI.unescape(params[:url])
+    topic = ShareTracker.get(url) || Topic.find_by_shortcode(params[:short_code])
+    
+    Shares::Recommend.create \
+      :topic => topic,
+      :user => current_user,
+      :visitor_code => visitor_code
+      
+    award_points
+    
+    @points_awarded = Points.allocators[:recommending]
+      
+    respond_to do |wants|
+      wants.js { render "points" }
     end
-    # url   = CGI.unescape(params[:url])
-    #     topic = ShareTracker.get(url) || Topic.find_by_shortcode(params[:short_code])
-    #     
-    #     Shares::Recommend.create \
-    #       :topic => topic,
-    #       :user => current_user,
-    #       :visitor_code => visitor_code
-    #       
-    #     award_points
-    #     
-    #     @points_awarded = Points.allocators[:recommending]
-    #       
-    #     respond_to do |wants|
-    #       wants.js { render "points" }
-    #     end
     
   end
   
