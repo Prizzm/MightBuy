b4f3dba68c78b58cf78981d126f3388f4a755afb
Sam Baumgarten <sbaumgarten@makrr.com>
2012-07-18 17:36:28 -0700

    added_image_from_twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -76,7 +76,7 @@ class User < ActiveRecord::Base
       puts "no uid"
       u = User.new()
       u.name = auth.info.name
-      u.mobile_image_url = auth.info.image
+      u.image = app.fetch_url(auth.info.image)
       if auth.provider == "twitter" then
         u.twitter_uid = auth.uid
       elsif auth.provider == "twitter" then
