077f00723e1b3cd6d0d780dac30c1b07d0599ac2
Sam Baumgarten <sbaumgarten@makrr.com>
2012-07-19 22:24:46 -0700

    facebook test

--- app/controllers/social_controller.rb
+++ app/controllers/social_controller.rb
@@ -3,11 +3,16 @@ class SocialController < ApplicationController
   skip_filter :award_points
  
  def askfriends
-   me = FbGraph::User.me("AAAGkC4pp4T0BAJY5LiuWtfHTEUYnWPVNnmdqN0Nc38As0r3vVWhDlbsgDnOTNWiCGKzP1Pue523bEQ1rR6jmI7IW9QtF89Dn5oOYPgZDZD")
-   action = me.og_action!(
-          "mightbuy:might_buy",
-          :product => "http://mightbuy.it/topics/#{params[:sc]}"
-        )
+   if current_user.facebook_uid
+     me = FbGraph::User.me("AAAGkC4pp4T0BAJY5LiuWtfHTEUYnWPVNnmdqN0Nc38As0r3vVWhDlbsgDnOTNWiCGKzP1Pue523bEQ1rR6jmI7IW9QtF89Dn5oOYPgZDZD")
+     action = me.og_action!(
+            "mightbuy:might_buy",
+            :product => "http://mightbuy.it/topics/#{params[:sc]}"
+          )
+     redirect_to "http://mightbuy.it/topics/#{params[:sc]}"
+   else
+     redirect_to "/users/auth/facebook"
+   end
  end
   
   # def askfriends
