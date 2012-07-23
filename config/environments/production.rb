d787853aa498766c5fdf89d9448e3fe1ca7529d3
Sam Baumgarten <sbaumgarten@makrr.com>
2012-07-22 20:17:04 -0700

    debug mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 NewPrizzmCom::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  FbGraph.debug!
   # Code is not reloaded between requests
   config.cache_classes = true
 
