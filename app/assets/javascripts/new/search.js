593a76c8b39419794add5a75126cf09ea61cf4f0
Amit Ambardekar <amitamb@gmail.com>
2012-08-18 17:44:50 +0530

    Improved styling for search page

--- app/assets/javascripts/new/search.js
+++ app/assets/javascripts/new/search.js
@@ -3,7 +3,7 @@ var POINTS_ENABLED = null;
 
 function updateScraperInfo($elem){
 
-  $elem.css({ "display" : "block" });
+  $elem.css({ "visibility" : "visible" });
 
   (function($elem, $next){
     $.getJSON("http://mightbuy-scraper.herokuapp.com/?callback=?",{url:$elem.data("url")},function(r){
@@ -14,7 +14,7 @@ function updateScraperInfo($elem){
         $elem.find(".thumb img").attr("src", r.images[0]);
       }
 
-      $elem.find(".public-search-result-scraper-info").css({ "display" : "block" });
+      $elem.find(".public-search-result-scraper-info").css({ "visibility" : "visible" });
 
       if ($next.length > 0) { updateScraperInfo($next); }
 
@@ -39,29 +39,29 @@ $(function(){
     var apiURL = window.location.protocol + '//ajax.googleapis.com/ajax/services/search/web?v=1.0&callback=?';
     $.getJSON(apiURL,{q:q,rsz:2,start:0},function(r){
       var res = r.responseData.results;
-      var result_elem = $("<ul></ul>");
+      var result_elem = $("<table></table>");
       if ( res.length ) {
         res = $.each(res, function(i, r){
 
-          var new_item = $('<div class="public-search-result clearfix" style="display:none;" data-url="'+ r.unescapedUrl +'" >' + 
-                              '<div class="public-search-result-google-info" style="float:left;width:500px;padding:10px;">' +
+          var new_item = $('<tr class="public-search-result" style="visibility:hidden;" data-url="'+ r.unescapedUrl +'" >' + 
+                              '<td class="public-search-result-google-info" >' +
                                 '<a class="title" target="_blank" href="'+ r.unescapedUrl +'">'+ r.titleNoFormatting +'</a>' +
                                 '<div class="description">'+ r.content +'</div>' +
                                 '<div class="url">'+ r.visibleUrl +'</div>' +
                                 '<a href="/topics/new?topic[subject]='+ encodeURIComponent(r.titleNoFormatting) +'&topic[url]='+ encodeURIComponent(r.unescapedUrl) +'">Might Buy</a>' +
-                              '</div>' +
-                              '<div class="public-search-result-scraper-info" style="display:none;float:left;width:180px;border-left:1px solid #444;padding:10px;">' +
+                              '</td>' +
+                              '<td class="public-search-result-scraper-info" style="visibility:hidden;">' +
                                 '<div class="thumb" ><img src="/assets/noimage.png" style="max-width:50px;max-height:50px;"></div>' +
                                 '<div class="price"><i>Price not available</i></div>' +
-                              '</div>' +
-                           '</div>');
+                              '</td>' +
+                           '</tr>');
 
           result_elem.append(new_item);
 
         });
 
         $(".public-search-results").html(result_elem.html());
-        $(".public-search-results .public-search-result:first").css({"display":"block"});
+        $(".public-search-results .public-search-result:first").css({"visibility":"visible"});
         updateScraperInfo($(".public-search-results .public-search-result:first"));
 
       }
