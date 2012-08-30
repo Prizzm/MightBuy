baa000fc646e811b866f6d0aeb6a5e27e33ef196
Amit Ambardekar <amitamb@gmail.com>
2012-08-31 02:34:06 +0530

    Topic subject suggestion done and image selection

--- app/assets/javascripts/new/image_selector.js
+++ app/assets/javascripts/new/image_selector.js
@@ -51,40 +51,29 @@
       image.find('td').html(img);
     }
 
-    if ( typeof func_or_options == "String" ) {
-      // handle the function with params
-      var func = func_or_options;
-      if ( func == "setImages" ){
-        setImages(params);
-      }
-    }
-    else {
+    // use the provided options
+    var options = func_or_options || {}	;
 
-      // use the provided options
-      var options = func_or_options || {}	;
+    // initialize handlers
+    var change_handler = options.change || function(){};
 
-      // initialize handlers
-      var change_handler = options.change || function(){};
+    this.addClass("image-selector");
+    this.html('<div class="image-holder"><a class="image blank" href="javascript:void(0);"><table class="centered" cellspacing="0" cellpadding="0"><tr><td></td></tr></table></a><div class="controls"><a class="prev" href="#prev">Previous</a><a class="next" href="#next">Next</a><a class="cancel" href="#cancel">Cancel</a></div></div>');
 
-      this.addClass("image-selector");
-      this.html('<div class="image-holder"><a class="image blank" href="javascript:void(0);"><table class="centered" cellspacing="0" cellpadding="0"><tr><td></td></tr></table></a><div class="controls"><a class="prev" href="#prev">Previous</a><a class="next" href="#next">Next</a><a class="cancel" href="#cancel">Cancel</a></div></div>');
+    var position          = 0;
+    var images            = [];
+    var image             = this.find('a.image');
+	var grab              = this.find('a.get');
+    var next              = this.find('a.next');
+    var prev              = this.find('a.prev');
+    var cancel            = this.find('a.cancel');
+    var blank_image_html  = '<table class="centered" cellspacing="0" cellpadding="0"><tbody><tr><td></td></tr></tbody></table>';
 
-      var position          = 0;
-      var images            = [];
-      var image             = this.find('a.image');
-	  var grab              = this.find('a.get');
-      var next              = this.find('a.next');
-      var prev              = this.find('a.prev');
-      var cancel            = this.find('a.cancel');
-      var blank_image_html  = '<table class="centered" cellspacing="0" cellpadding="0"><tbody><tr><td></td></tr></tbody></table>';
-
-      next.click(function () { return gonext(); })
-      prev.click(function () { return goprev(); })
-      cancel.click(function () { return gocancel(); }) 
+    next.click(function () { return gonext(); })
+    prev.click(function () { return goprev(); })
+    cancel.click(function () { return gocancel(); }) 
       
-      setImages(options.images || []);
-
-    }
+    setImages(options.images || []);
 
   };
 })( jQuery );
--- app/assets/javascripts/new/item_search.js
+++ app/assets/javascripts/new/item_search.js
@@ -1,10 +1,38 @@
 $(function(){
   
-  
-  
+  $("#topic_subject").autocomplete({
+    source : function(request, response){
+      var apiURL = 'http://suggestqueries.google.com/complete/search?hl=en&client=youtube&hjson=t&cp=1&key=&format=5&alt=json&callback=?';
+      $.getJSON(apiURL,{q:request.term},function(r){
+		var res = r[1];
+		if ( res.length ) {
+          res = $.map(res.slice(0, 4), function(n, i){
+            return { label : n[0], value : n[0] };
+          });
+        }
+        response(res);
+      });
+	},
+    open: function(event, ui) {
+    },
+    close: function(event, ui) {
+    },
+    select : function(event, ui){
+      $(".show-google-search").click();
+    },
+    appendTo : "#item_subject_search_autocomplete"
+  });
+
+  $("#topic_subject").keypress(function(e) { 
+    if(e.which == 13) { 
+      $(".show-google-search").click();
+      e.preventDefault();
+    } 
+  });
+
   $(".show-google-search").click(function(){
     var apiURL = window.location.protocol + '//ajax.googleapis.com/ajax/services/search/web?v=1.0&callback=?';
-    $.getJSON(apiURL,{q:$("#topic_subject").val(),rsz:3,start:0},function(r){ // $("#topic_subject").val()
+    $.getJSON(apiURL,{q:$("#topic_subject").val(),rsz:4,start:0},function(r){ // $("#topic_subject").val()
       var res = r.responseData.results;
       var result_elem = $("<ul></ul>");
       if ( res.length ) {
@@ -26,19 +54,47 @@ $(function(){
       {
         $("#gsearch-results").html("<br />No results found<br /><br />");
       }
-      $.fancybox.resize();
-      $.fancybox.center();
+      $("#gsearch-results").css({ "display" : "" });
+      $("#item-form-image-selector").css({ "display" : "none" });
     });
     
     return false;
-    
+  });
+
+  $("#gsearch-results").on("close", function(){
+    $(this).css({ "display" : "none" });
+    $("#item-form-image-selector").css({ "display" : "" });
   });
 
   $(document).on("click", ".g_result a", function(e){
     $("#topic_url").val($(this).attr("href"));
+    $("#topic_url").attr("old_val", "_BLANK");
+    $("#gsearch-results").trigger("close");
     $("#topic_url").trigger("complete");
     e.preventDefault();
-    $.fancybox.close();
+  });
+
+  $(document).on("mouseenter", ".g_result a", function(e){
+    $("#topic_url").attr("old_val", $("#topic_url").val());
+    $("#topic_url").val($(this).attr("href"));
+    e.preventDefault();
+  }).on("mouseleave", ".g_result a", function(e){
+    if ( $("#topic_url").attr("old_val") != "_BLANK" ) {
+      $("#topic_url").val($("#topic_url").attr("old_val"));
+      $("#topic_url").attr("old_val", "");
+    }
+  });
+
+  var initial_images = [];
+  
+  if ( $("#topic_image_url").val() ) {
+    initial_images.push($("#topic_image_url").val());
+  }
+
+  $("#item-form-image-selector").imageSelector({
+    change : function(image){
+    },
+    images : initial_images
   });
 
   // TODO : Supporting method possibly useful elsewhere as well
@@ -48,22 +104,10 @@ $(function(){
     e.preventDefault();
   });
 
-  $("#topic_subject").bind("keyup", function(){
-    var subject = $(this).val();
-    if ( $.trim(subject) == "" ) {
-      subject = "&lt;search term&gt;"
-    }
-    $(".show-google-search a").html("Search Google for </br> \"<name>\"".replace("<name>", subject));
-  });
-  $("#topic_subject").trigger("keyup");
-
-  $("#topic_subject").focusout(function(){
-    
-  });
-
   $("#topic_url").bind("keypress", function(e){
     if ( e.which == 13 ) {
-       e.preventDefault();
+      $("#topic_url").trigger("complete");
+      e.preventDefault();
     }
   });
 
@@ -80,25 +124,39 @@ $(function(){
       }, 0);
     }
   });
+  
+  $("#topic_url").focus(function(){
+    $("#gsearch-results").trigger("close");
+  });
 
   $("#topic_url").bind("complete", function(){
     // set grab image URL
-    $(".image-selector-uploader #web input").val($("#topic_url").val());
-    $(".image-selector-uploader #web .get").click();
+    var url = $("#topic_url").val();
+    $.getJSON(window.location.protocol + '//mightbuy-scraper.herokuapp.com/?url='+ encodeURIComponent(url) +'&callback=?', function(data){
+      var images = data.images;
+      
+      $("#item-form-image-selector").imageSelector({
+        change : function(image){
+          $("#topic_image_url").val(image);
+        },
+        images : images
+      });
+      
+      if ( data.price ) { $("#topic_price").val(data.price); }
+    });
   });
 
   // if URL is not blank show grab image dialog immediately
   // used in bookmarklet
   if ( $("#topic_url").val() ) {
-   	if ($(".item-more-options .image-holder img").length == 0) { // no image exists
-        // no image
-	      $("#topic_url").trigger("complete");
-	    } else {
-	      // set grab image URL
-	      $(".image-selector-uploader #web input").val($("#topic_url").val());
-	    }
+   	if ($("#item-form-image-selector .image-holder img").length == 0) { // no image exists
+      // no image
+	  $("#topic_url").trigger("complete");
+	} else {
+	  // there is already an image
+	}
   }
-  
+
   // everything below is commented
   return;
 
