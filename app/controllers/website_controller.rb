517bb1ccb823396e8671638d2c84ef554f19e022
Hemant Kumar <gethemant@gmail.com>
2012-09-08 00:04:17 +0530

    Remove older links

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -1,21 +1,16 @@
-class WebsiteController < ApplicationController  
-  
-  def about
-    render :layout => "application"
-  end
-  
+class WebsiteController < ApplicationController
   def index
     if current_user
       redirect_to "/me"
     end
   end
-  
+
   private
-  
+
   helper_method :brand?
-  
+
   def brand?
     params[:shopper] != true
   end
-  
+
 end
