b7787e00517e0b5b99031cb765692555e0bea977
Yuva <yuva@codemancers.com>
2012-09-07 23:50:25 +0530

    WIP: fb login, adding design n stuff

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
-  before_filter :logS
-
-  def logS
-    puts "session: ", session["devise.user_attributes"]
-  end
+  layout "welcomes"
 
   private
-
   def build_resource(hash=nil)
     hash ||= params[resource_name] || {}
     hash.merge! :visitor_code => visitor_code
