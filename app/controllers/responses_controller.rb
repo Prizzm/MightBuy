7c18c256bdb4747a1b5d047286081fccd4091ea0
Yuva <yuva@codemancers.com>
2012-09-10 20:27:51 +0530

    rewrite responses controller

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,53 +1,24 @@
-class ResponsesController < InheritedResources::Base
-  # Relationships
-  belongs_to :topic, :finder => :find_by_shortcode!
-  
-  def index
-    index! do |format|
-      format.js
-    end
-  end
-  
+class ResponsesController < ApplicationController
+  before_filter :find_topic!
+  respond_to :html, :js
+
   def create
-    @repsonse = build_resource
-    @response.user = current_user
-    @response.visitor_code = visitor_code
-    @response.share = related_share
-    
-    create! do |success, failure|
-      success.html { redirect_to topic_path(parent) }
-      success.js do
-        case parent.form
-          when "recommendation"
-            render "responses/recommendation/create"
-          else render "create"
-        end
-      end
-      success.js
-      failure.js { render 'error' }
-    end
-  end
-  
-  private
-  
-    def filter
-      case params[:filter_by]
-        when 'unreplied' then end_of_association_chain.unreplied
-        when 'recommended' then end_of_association_chain.recommended
-        when 'undecided' then end_of_association_chain.undecided
-        when 'not_recommended' then end_of_association_chain.not_recommended
-        else end_of_association_chain
-      end
-    end
-  
-    def collection
-      @responses ||= filter.includes(:replies).where(:reply_id => nil)
-    end
-  
-    def related_share
-      if !current_user && cookies.has_key?(:share_code)
-        @related_share ||= Shares::Share.find_by_shortcode(cookies[:share_code])
-      end
+    @topic_response = @topic.responses.build(params[:response])
+    @topic_response.user = current_user
+
+    if @topic_response.save && current_user.nil?
+      session[:response_id]  = @topic_response.id
+      session[:redirect_path] = topic_path(@topic, r: :t)
     end
 
+    location_path = current_user ? topic_path(@topic, r: :t) : new_user_session_path
+    respond_with(@topic, location: location_path)
+  end
+
+  private
+  def find_topic!
+    unless @topic = Topic.find_by_shortcode(params[:topic_id])
+      respond_with(@topic, location: root_path)
+    end
+  end
 end
