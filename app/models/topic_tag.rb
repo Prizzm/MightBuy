f3e2586c393a1905851d3c54c5055d523f0ae284
Hemant Kumar <gethemant@gmail.com>
2012-09-10 11:41:59 +0530

    Check in tags migration

---
+++ app/models/topic_tag.rb
@@ -0,0 +1,3 @@
+class TopicTag < ActiveRecord::Base
+  attr_accessible :tag_id, :topic_id
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120823191506) do
+ActiveRecord::Schema.define(:version => 20120910060743) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -207,6 +207,12 @@ ActiveRecord::Schema.define(:version => 20120823191506) do
   end
 
   add_index "shares", ["shortcode"], :name => "index_shares_on_shortcode", :unique => true
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "topics", :force => true do |t|
     t.integer  "user_id"
