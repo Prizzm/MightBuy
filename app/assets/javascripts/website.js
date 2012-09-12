9eafafc6b4766209d68c133d6acf392c196c3c37
Hemant Kumar <gethemant@gmail.com>
2012-09-12 10:34:52 +0530

    cleanup javascript

--- app/assets/javascripts/website.js
+++ app/assets/javascripts/website.js
@@ -1,9 +1,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require vendor/selectivizr.js
-//= require vendor/jquery.qtip.js
-//= require vendor/jquery.placeholder.js
+//= require selectivizr.js
+//= require jquery.qtip.js
+//= require jquery.placeholder.js
 //= require new/website.js
 //= require mightbuy
 //= require ajax_notifier
