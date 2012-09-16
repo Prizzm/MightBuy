9a42e696cf0b592e6c74fd92adafdd7dabd3b689
Hemant Kumar <gethemant@gmail.com>
2012-09-16 18:16:01 +0530

    Update version of jQuery.pageless being used

--- vendor/assets/javascripts/jquery.pageless.js
+++ vendor/assets/javascripts/jquery.pageless.js
@@ -1,1 +1,194 @@
-(function(c){var m=!1,d=!m,a,o=m,g,h=".pageless",k="scroll"+h,e="resize"+h,q={container:window,currentPage:1,distance:100,pagination:".pagination",params:{},url:location.href,loaderImage:"/images/load.gif"},i,n;c.pageless=function(t){c.isFunction(t)?q.call():p(t)};var f=function(){return q.loaderHtml||'<div id="pageless-loader" style="display:none;text-align:center;width:100%;">  <div class="msg" style="color:#e9e9e9;font-size:2em"></div>  <img src="'+q.loaderImage+'" alt="loading more results" style="margin:10px auto" /></div>'};var p=function(t){if(q.inited){return}q.inited=d;if(t){c.extend(q,t)}i=q.container;n=c(i);if(q.pagination){c(q.pagination).remove()}l()};c.fn.pageless=function(v){var u=c(this),t=c(v.loader,u);p(v);a=u;if(v.loader&&t.length){g=t}else{g=c(f());u.append(g);if(!v.loaderHtml){c("#pageless-loader .msg").html(v.loaderMsg)}}};var b=function(t){(o=t)?(g&&g.fadeIn("normal")):(g&&g.fadeOut("normal"))};var s=function(){return(i===window)?c(document).height()-n.scrollTop()-n.height():n[0].scrollHeight-n.scrollTop()-n.height()};var r=function(){n.unbind(h)};var l=function(){n.bind(k+" "+e,j).trigger(k)};var j=function(){if(q.totalPages<=q.currentPage){r();if(q.end){q.end.call()}return}if(!o&&(s()<q.distance)){b(d);q.currentPage++;c.extend(q.params,{page:q.currentPage});c.get(q.url,q.params,function(t){c.isFunction(q.scrape)?q.scrape(t):t;g?g.before(t):a.append(t);b(m);if(q.complete){q.complete.call()}})}}})(jQuery);
+// =======================================================================
+// PageLess - endless page
+//
+// Pageless is a jQuery plugin.
+// As you scroll down you see more results coming back at you automatically.
+// It provides an automatic pagination in an accessible way : if javascript 
+// is disabled your standard pagination is supposed to work.
+//
+// Licensed under the MIT:
+// http://www.opensource.org/licenses/mit-license.php
+//
+// Parameters:
+//    currentPage: current page (params[:page])
+//    distance: distance to the end of page in px when ajax query is fired
+//    loader: selector of the loader div (ajax activity indicator)
+//    loaderHtml: html code of the div if loader not used
+//    loaderImage: image inside the loader
+//    loaderMsg: displayed ajax message
+//    pagination: selector of the paginator divs. 
+//                if javascript is disabled paginator is provided
+//    params: paramaters for the ajax query, you can pass auth_token here
+//    totalPages: total number of pages
+//    url: URL used to request more data
+//
+// Callback Parameters:
+//    scrape: A function to modify the incoming data.
+//    complete: A function to call when a new page has been loaded (optional)
+//    end: A function to call when the last page has been loaded (optional)
+//
+// Usage:
+//   $('#results').pageless({ totalPages: 10
+//                          , url: '/articles/'
+//                          , loaderMsg: 'Loading more results'
+//                          });
+//
+// Requires: jquery
+//
+// Author: Jean-Sébastien Ney (https://github.com/jney)
+//
+// Contributors:
+//   Alexander Lang (https://github.com/langalex)
+//   Lukas Rieder (https://github.com/Overbryd)
+//   Kathryn Reeve (https://github.com/BinaryKitten)
+//
+// Thanks to:
+//  * codemonky.com/post/34940898
+//  * www.unspace.ca/discover/pageless/
+//  * famspam.com/facebox
+// =======================================================================
+
+(function($) {
+  
+  var FALSE = !1
+    , TRUE = !FALSE
+    , element
+    , isLoading = FALSE
+    , loader
+    , namespace = '.pageless'
+    , SCROLL = 'scroll' + namespace
+    , RESIZE = 'resize' + namespace
+    , settings = { container: window
+                 , currentPage: 1
+                 , distance: 100
+                 , pagination: '.pagination'
+                 , params: {}
+                 , url: location.href
+                 , loaderImage: "/images/load.gif"
+                 }
+    , container
+    , $container;
+    
+  $.pageless = function(opts) {
+    $.isFunction(opts) ? opts.call() : init(opts);
+  };
+  
+  $.pagelessReset = function() {
+    settings = { container: window
+                 , currentPage: 1
+                 , distance: 100
+                 , pagination: '.pagination'
+                 , params: {}
+                 , url: location.href
+                 , loaderImage: "/images/load.gif"
+                 };
+    stopListener();
+      // if there is a afterStopListener callback we call it
+    if (settings.end) settings.end.call();
+  };
+  
+  var loaderHtml = function () {
+    return settings.loaderHtml || '\
+<div id="pageless-loader" style="display:none;text-align:center;width:100%;">\
+  <div class="msg" style="color:#e9e9e9;font-size:2em"></div>\
+  <img src="' + settings.loaderImage + '" alt="loading more results" style="margin:10px auto" />\
+</div>';
+  };
+ 
+  // settings params: totalPages
+  var init = function (opts) {
+    if (settings.inited) return;
+    settings.inited = TRUE;
+    
+    if (opts) $.extend(settings, opts);
+    
+    container = settings.container;
+    $container = $(container);
+    
+    // for accessibility we can keep pagination links
+    // but since we have javascript enabled we remove pagination links 
+    if(settings.pagination) $(settings.pagination).remove();
+    
+    // start the listener
+    startListener();
+  };
+  
+  $.fn.pageless = function (opts) {
+    var $el = $(this)
+      , $loader = $(opts.loader, $el);
+      
+    init(opts);
+    element = $el;
+    
+    // loader element
+    if (opts.loader && $loader.length) {
+      loader = $loader;
+    } else {
+      loader = $(loaderHtml());
+      $el.append(loader);
+      // if we use the default loader, set the message
+      if (!opts.loaderHtml) {
+        $('#pageless-loader .msg').html(opts.loaderMsg).css(opts.msgStyles || {});
+      }
+    }
+  };
+  
+  //
+  var loading = function (bool) {
+    (isLoading = bool)
+    ? (loader && loader.fadeIn('normal'))
+    : (loader && loader.fadeOut('normal'));
+  };
+  
+  // distance to end of the container
+  var distanceToBottom = function () {
+    return (container === window)
+         ? $(document).height() 
+         - $container.scrollTop() 
+         - $container.height()
+         : $container[0].scrollHeight 
+         - $container.scrollTop() 
+         - $container.height();
+  };
+
+  var stopListener = function() {
+    $container.unbind(namespace);
+  };
+  
+  // * bind a scroll event
+  // * trigger is once in case of reload
+  var startListener = function() {
+    $container.bind(SCROLL+' '+RESIZE, watch)
+              .trigger(SCROLL);
+  };
+  
+  var watch = function() {
+    // listener was stopped or we've run out of pages
+    if (settings.totalPages <= settings.currentPage) {
+      stopListener();
+      // if there is a afterStopListener callback we call it
+      if (settings.end) settings.end.call();
+      return;
+    }
+    
+    // if slider past our scroll offset, then fire a request for more data
+    if(!isLoading && (distanceToBottom() < settings.distance)) {
+      loading(TRUE);
+      // move to next page
+      settings.currentPage++;
+      // set up ajax query params
+      $.extend( settings.params
+              , { page: settings.currentPage });
+      // finally ajax query
+      $.get( settings.url
+           , settings.params
+           , function (data) {
+               $.isFunction(settings.scrape) ? settings.scrape(data) : data;
+               loader ? loader.before(data) : element.append(data);
+               loading(FALSE);
+               // if there is a complete callback we call it
+               if (settings.complete) settings.complete.call();
+           }, 'html');
+    }
+  };
+})(jQuery);
