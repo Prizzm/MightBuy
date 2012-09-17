065000ded569108dac3ba071f8933b9683e1d7a4
Yuva <yuva@codemancers.com>
2012-09-17 20:27:30 +0530

    add tests to make sure email is sent

---
+++ spec/observers/share_observer_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe ShareObserver do
+  after (:each) do
+    Notifications.deliveries.clear
+  end
+
+  it "should send an email if shares email is created" do
+    Notifications.deliveries.should be_empty
+
+    email_share = FactoryGirl.create(:email_share)
+    Notifications.deliveries.should_not be_empty
+    mail = Notifications.deliveries.first
+
+    topic = email_share.topic
+    subject = "#{topic.user.name} might buy #{topic.subject}"
+    mail.subject.should match(/#{subject}/)
+    mail.to.should include(email_share.with)
+  end
+end
