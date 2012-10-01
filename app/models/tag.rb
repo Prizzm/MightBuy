d963160b812c1ce685115a850d3acff0c20a5ac3
Hemant Kumar <gethemant@gmail.com>
2012-10-01 22:40:33 +0530

    Add order by clause for tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -9,7 +9,7 @@ class Tag < ActiveRecord::Base
   validates_uniqueness_of :name
 
   def self.popular_tags
-    Tag.select("tags.*, c.tag_count").joins("JOIN (select topic_tags.tag_id, count(*) as tag_count from topic_tags group by tag_id) c on tags.id = c.tag_id").limit(15)
+    Tag.select("tags.*, c.tag_count").joins("JOIN (select topic_tags.tag_id, count(*) as tag_count from topic_tags group by tag_id) c on tags.id = c.tag_id").order("c.tag_count desc").limit(15)
   end
 
   def to_param
