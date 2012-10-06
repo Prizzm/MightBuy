d91b8bb3cd8e19ae44d82fd0b4815bfa4f3a13c1
Hemant Kumar <gethemant@gmail.com>
2012-10-06 13:23:49 +0530

    remove i have link from topic show page

--- app/controllers/haves_controller.rb
+++ app/controllers/haves_controller.rb
@@ -1,5 +1,7 @@
 class HavesController < ApplicationController
   layout :choose_layout
+  before_filter :find_topic!, only: [:show, :update, :edit, :destroy, :bought]
+
   def index
     @haves = current_user.topics.have
     @selected_tab = "ihave"
@@ -10,8 +12,24 @@ class HavesController < ApplicationController
     @selected_tab = "ihave"
   end
 
+  def edit
+
+  end
+
+  def destroy
+    @topic.destroy
+    flash[:notice] = "The item has been removed"
+    redirect_to profile_path()
+  end
+
   private
   def choose_layout
     current_user ? 'logged_user' : 'anonymous'
+  end
+
+  def find_topic!
+    unless @topic = Topic.find_by_shortcode(params[:topic_id])
+      respond_with(@topic, location: root_path)
+    end
   end
 end
