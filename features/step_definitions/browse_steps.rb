dcdf239469154dd1cb12ee172e1e5bd92694c41c
Hemant Kumar <gethemant@gmail.com>
2012-10-13 13:17:18 +0530

    Fix wording and fix all the features that started to break because of
    wording change

--- features/step_definitions/browse_steps.rb
+++ features/step_definitions/browse_steps.rb
@@ -1,9 +1,9 @@
 Then /^I should see my empty feed$/ do
   page.should have_content("My Activity feed")
-  page.should have_content("My wants")
+  page.should have_content("I Might Buy")
 end
 
 Then /^I should see "(.*?)" feeds$/ do |name|
   page.should have_content("#{name}'s Activity feed")
-  page.should have_content("#{name}'s wants")
+  page.should have_content("#{name} Might Buy")
 end
