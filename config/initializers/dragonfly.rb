b888b5e0378c3145a5c06d805a9aef6b376d3cb1
Hemant Kumar <gethemant@gmail.com>
2012-10-20 21:49:19 +0530

    remove app url configuration

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -13,7 +13,6 @@ if Rails.env.production?
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     )
-    c.url_host = MB.config.app_url
   end
 end
 
