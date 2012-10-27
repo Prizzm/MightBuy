b7e74a22efced600ba133f0f0909166600daa6dd
Yuva <yuva@codemancers.com>
2012-10-27 18:45:26 +0530

    WIP: comments mailer should consider receiver into account

--- app/mailers/comments_mailer.rb
+++ app/mailers/comments_mailer.rb
@@ -1,13 +1,13 @@
 class CommentsMailer < ActionMailer::Base
   default from: "no-reply@mightbuy.it"
 
-  def email_topic_owner(comment)
+  def email_topic_participant(participant, comment)
     @comment   = comment
     @commenter = comment.user
-    @receiver  = comment.topic.user
+    @receiver  = participant
 
     from    = "#{@commenter.name.capitalize} <no-reply@mightbuy.it>"
-    subject = "[MB] You have received new comment on '#{@comment.topic.subject.first(45)}'"
+    subject = "[MB] New comment on '#{@comment.topic.subject.first(45)}'"
     mail to: @receiver.email, from: from, subject: subject
   end
 end
--- spec/mailers/comments_mailer_spec.rb
+++ spec/mailers/comments_mailer_spec.rb
@@ -4,7 +4,8 @@ describe CommentsMailer do
   describe ".email_topic_owner" do
     context "when a comment is supplied to formulate an email" do
       let(:comment) { FactoryGirl.create(:comment) }
-      let(:mail)    { CommentsMailer.email_topic_owner(comment) }
+      let(:topic)   { comment.topic }
+      let(:mail)    { CommentsMailer.email_topic_participant(topic.user, comment) }
 
       it "has commenter's name in from" do
         from_field = mail[:from].to_s
@@ -12,15 +13,15 @@ describe CommentsMailer do
       end
 
       it "has receiver's email in to" do
-        mail.to.should include(comment.topic.user.email)
+        mail.to.should include(topic.user.email)
       end
 
       it "has subject according to topic" do
-        mail.subject.should match(comment.topic.subject.first(45))
+        mail.subject.should match(topic.subject.first(45))
       end
 
       it "has topic url in the body" do
-        mail.body.should match(topic_url(comment.topic))
+        mail.body.should match(topic_url(topic))
       end
     end
   end
