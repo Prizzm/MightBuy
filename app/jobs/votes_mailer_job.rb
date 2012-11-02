fc19d9cc7d2b774862239ded4817fac090043c5f
Yuva <yuva@codemancers.com>
2012-11-02 16:01:22 +0530

    topic owner shouldnot get any email when he himself votes

--- app/jobs/votes_mailer_job.rb
+++ app/jobs/votes_mailer_job.rb
@@ -5,7 +5,7 @@ class VotesMailerJob < Struct.new(:vote_id)
 
   def perform
     if vote && vote.user && vote.topic && vote.topic.user
-      VotesMailer.email_topic_owner(vote).deliver
+      VotesMailer.email_topic_owner(vote).deliver if vote.user != vote.topic.user
     end
   end
 end
