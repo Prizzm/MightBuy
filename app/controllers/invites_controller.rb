b6f0eca5165a9e489d4d38ff82f8ff36d7dcd407
Hemant Kumar <gethemant@gmail.com>
2012-11-09 01:36:14 +0530

    New page for lead invites is working

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,9 +1,16 @@
 class InvitesController < ApplicationController
 
   before_filter :find_user_with_invite_token
+  layout 'logged_user'
 
   def show
-    @topic = @user.topics.find(params[:topic_id])
+    @topic = @user.topics.find_by_shortcode(params[:topic_id])
+    @comments = @topic.ordered_comments
+    @comment = Comment.new
+
+    if current_user && @topic.owner?(current_user)
+      @selected_tab = 'mightbuy'
+    end
   end
 
   private
