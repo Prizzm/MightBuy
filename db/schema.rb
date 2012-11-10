bd36b613b8d202983089255275000b6af76a26af
Hemant Kumar <gethemant@gmail.com>
2012-11-10 14:36:33 +0530

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121103140056) do
+ActiveRecord::Schema.define(:version => 20121109063845) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -185,10 +185,17 @@ ActiveRecord::Schema.define(:version => 20121103140056) do
   create_table "customer_leads", :force => true do |t|
     t.string   "email"
     t.string   "name"
-    t.string   "status"
+    t.string   "status",                                     :default => "notsent"
     t.integer  "product_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                                                        :null => false
+    t.datetime "updated_at",                                                        :null => false
+    t.integer  "business_id",                                                       :null => false
+    t.string   "phone_number"
+    t.boolean  "join_list",                                  :default => false
+    t.string   "photo_uid"
+    t.decimal  "price",        :precision => 6, :scale => 2
+    t.string   "invite_token"
+    t.integer  "user_id"
   end
 
   create_table "deal_deals", :force => true do |t|
@@ -240,6 +247,18 @@ ActiveRecord::Schema.define(:version => 20121103140056) do
     t.string   "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "lead_configs", :force => true do |t|
+    t.integer  "business_id"
+    t.boolean  "include_liability",      :default => false
+    t.text     "liability",              :default => ""
+    t.boolean  "ask_for_name",           :default => false
+    t.boolean  "ask_to_join_list",       :default => false
+    t.boolean  "include_product_select", :default => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.boolean  "ask_for_phonenumber",    :default => false
   end
 
   create_table "orders", :force => true do |t|
