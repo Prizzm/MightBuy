ba50a942f1521b3cc6c6a727f13a26df167af15e
Iszak Bryan <iszak@karmarama.com>
2012-11-20 22:49:42 +0000

    Migrated the dashboard to use active admins new methods

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,61 +1,32 @@
-ActiveAdmin::Dashboards.build do
+ActiveAdmin.register_page "Dashboard" do
+  menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
-  # Define your dashboard sections here. Each block will be
-  # rendered on the dashboard in the context of the view. So just
-  # return the content which you would like to display.
-  
-  section "Recent Beta Signups" do
-    table do
-      tr do
-        th "Email"
-        th "At"
+  content :title => proc{ I18n.t("active_admin.dashboard") } do
+    columns do
+      column do
+        panel "Products Added" do
+          h2 Product.all.count
+        end
       end
-      tr do
-        BetaSignup.recent(5).collect do |signup|
-          td link_to signup.email_address, admin_beta_signup_path(signup)
-          td signup.created_at
+
+      column do
+        panel "Recent Beta Signups" do
+          table_for BetaSignup.recent(5) do
+            column "Email" do |item|
+              item.email
+            end
+            column "At" do |item|
+              item.created_at
+            end
+          end
+        end
+      end
+
+      column do
+        panel "Users Added" do
+          h2 User.all.count
         end
       end
     end
   end
-  
-  section "Products Added" do
-      h2 Product.all.count
-  end
-  
-  section "Users Added" do
-      h2 User.all.count
-  end
-  
-  # == Simple Dashboard Section
-  # Here is an example of a simple dashboard section
-  #
-  #   section "Recent Posts" do
-  #     ul do
-  #       Post.recent(5).collect do |post|
-  #         li link_to(post.title, admin_post_path(post))
-  #       end
-  #     end
-  #   end
-  
-  # == Render Partial Section
-  # The block is rendered within the context of the view, so you can
-  # easily render a partial rather than build content in ruby.
-  #
-  #   section "Recent Posts" do
-  #     div do
-  #       render 'recent_posts' # => this will render /app/views/admin/dashboard/_recent_posts.html.erb
-  #     end
-  #   end
-  
-  # == Section Ordering
-  # The dashboard sections are ordered by a given priority from top left to
-  # bottom right. The default priority is 10. By giving a section numerically lower
-  # priority it will be sorted higher. For example:
-  #
-  #   section "Recent Posts", :priority => 10
-  #   section "Recent User", :priority => 1
-  #
-  # Will render the "Recent Users" then the "Recent Posts" sections on the dashboard.
-
 end
