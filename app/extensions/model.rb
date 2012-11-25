e9bb8acda2e144c030a82c3cc9854f9dfe22a096
Iszak Bryan <iszak@karmarama.com>
2012-11-25 02:26:20 +0000

    Fixed deprecated errors

--- app/extensions/model.rb
+++ app/extensions/model.rb
@@ -1,89 +1,79 @@
 module Model
-  
   module Many
-    
     extend ActiveSupport::Concern
-    
-    module ClassMethods
-      
-      def validates_many (name)
-        validate do
-          unless send(name).all?(&:valid?)
-            errors.add(name)
-          end
+
+
+    def self.validates_many(name)
+      validate do
+        unless send(name).all?(&:valid?)
+          errors.add(name)
         end
       end
-      
-      # validate do
-      #   unless emails.all?(&:valid?)
-      #     errors.add(:emails)
-      #   end
-      # end
-      
-      def many (method, options = {})
-        method = method.to_s
-        klass  = options[:class_name]
-        
-        define_method method do
-          @attributes[method] ||= []
-        end
-        
-        define_method "#{method}=" do |array_or_hash|
-          @attributes[method] = []
-          constant = options[:class_name].constantize
-          
-          case array_or_hash
-            when Array
-              array_or_hash.each do |attributes|
-                @attributes[method].push constant.new(attributes)
-              end
-            else
-              array_or_hash.each do |key, attributes|
-                @attributes[method].push constant.new(attributes)
-              end
-          end
-        end
-      end
-      
     end
-    
+
+
+    # validate do
+    #   unless emails.all?(&:valid?)
+    #     errors.add(:emails)
+    #   end
+    # end
+
+
+    def self.many(method, options = {})
+      method = method.to_s
+      klass  = options[:class_name]
+
+      define_method method do
+        @attributes[method] ||= []
+      end
+
+      define_method "#{method}=" do |array_or_hash|
+        @attributes[method] = []
+        constant = options[:class_name].constantize
+
+        case array_or_hash
+          when Array
+            array_or_hash.each do |attributes|
+              @attributes[method].push constant.new(attributes)
+            end
+          else
+            array_or_hash.each do |key, attributes|
+              @attributes[method].push constant.new(attributes)
+            end
+        end
+      end
+    end
   end
-  
+
+
+
   module Attributes
-    
     extend ActiveSupport::Concern
-    
+
+
     included do
       attr_accessor :attributes
     end
-    
-    module ClassMethods
-      
-      def attribute (name)
-        name = name.to_s
-        
-        define_method name do
-          @attributes[name]
-        end
-        
-        define_method "#{name}=" do |value|
-          @attributes[name] = value
-        end
+
+
+    def self.attribute(name)
+      name = name.to_s
+
+      define_method name do
+        @attributes[name]
       end
-      
-    end
-    
-    module InstanceMethods
-      
-      def initialize (attrs = {})
-        @attributes = {}
-        attrs.each do |key, value|
-          send("#{key}=", value) if respond_to?("#{key}=")
-        end
+
+      define_method "#{name}=" do |value|
+        @attributes[name] = value
       end
-      
     end
-    
+
+
+    def initialize(attrs = {})
+      @attributes = {}
+      attrs.each do |key, value|
+        send("#{key}=", value) if respond_to?("#{key}=")
+      end
+    end
   end
-  
 end
